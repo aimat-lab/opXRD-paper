52d5acbe13f2e17df62e27d1b9dadf103a927a3b
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-01-15 15:14:14 +0100

    data_collector: Moved Selection Layout to its own class

--- data_collector/app.py
+++ data_collector/app.py
@@ -8,10 +8,9 @@ from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
 
 from data_collector.filesystem import zip_file_list, produce_csv_file
-from data_collector.elements import NodeWidget, InputDialog, BlackLabel, ThickVerticalSlider, get_checkboxes_layout, \
-    get_scroll_view, get_scrollable_checkboxes_layout
-from data_collector.elements import get_header_widget, get_ok_button, get_feedback_widget
-from data_collector.configs import get_line_height
+from data_collector.elements import NodeWidget, InputDialog, BlackLabel
+from data_collector.elements import get_ok_button, get_feedback_widget
+from data_collector.elements import SelectionLayout
 
 
 # -------------------------------------------
@@ -30,42 +29,21 @@ class DataCollectApp(App):
 
         # PathCheckbox
         self.root_checkbox : Optional[NodeWidget] = None
-
-        # GUI Elements
-        self.slider : Optional[Widget] = None
         self.feedback_widget : Optional[Widget] = None
-        self.scroll_view : Optional[Widget] = None
-        self.header_layout : Optional[Widget] = None
+        self.selection_layout : Optional[SelectionLayout] = None
+
         self.target_path_input : Optional[Widget] = None
 
 
     def build(self):
-        self.scroll_view = get_scroll_view()
-        self.scroll_view.bind(scroll_y=self.on_scroll_view_scroll)
-
-        selection_layout = self.make_selection_layout()
+        self.selection_layout = SelectionLayout(rootCheckbox=self.root_checkbox)
         finish_layout = self.make_finish_layout()
 
         root_layout = BoxLayout(orientation='vertical')
-        root_layout.add_widget(selection_layout)
+        root_layout.add_widget(self.selection_layout)
         root_layout.add_widget(finish_layout)
 
         return root_layout
-
-
-    def make_selection_layout(self):
-        self.header_layout = get_header_widget(num_elements=0)
-        self.header_layout.opacity = 0
-        checkboxes_layout = get_checkboxes_layout(file_count_label=self.header_layout,
-                                                  scroll_view=self.scroll_view)
-        self.slider = ThickVerticalSlider(orientation='vertical', min=0, max=1, value=1, size_hint=(0.1, 1))
-        self.slider.bind(value=self.adjust_scroll_view)
-
-        selection_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.9))
-        selection_layout.add_widget(widget=checkboxes_layout)
-        selection_layout.add_widget(widget=self.slider)
-
-        return selection_layout
 
 
     def make_finish_layout(self):
@@ -97,23 +75,12 @@ class DataCollectApp(App):
 
         return finish_layout
 
-    def set_select_layout_content(self, path : str):
-        self.root_checkbox: NodeWidget = NodeWidget(path=path, height=get_line_height(), scroll_view=self.scroll_view)
-        self.root_checkbox.recursively_initialize_filestructure()
-
-        new_label = get_header_widget(num_elements=len(self.root_checkbox.xrd_file_des)).children[2]
-        self.header_layout.children[2].text =  new_label.text
-        self.header_layout.opacity = 1
-
-        scroll_layout = get_scrollable_checkboxes_layout(root_checkbox=self.root_checkbox)
-        self.scroll_view.add_widget(widget=scroll_layout)
-
 
     def on_start(self):
         if self.input_folder_override is None:
-            self.show_launch_dialog(callback=self.set_select_layout_content)
+            self.show_launch_dialog(callback=self.selection_layout.set_content)
         else:
-            self.set_select_layout_content(path=self.input_folder_override)
+            self.selection_layout.set_content(path=self.input_folder_override)
 
     @staticmethod
     def show_launch_dialog( callback : callable):
@@ -121,16 +88,7 @@ class DataCollectApp(App):
 
     # -------------------------------------------
     # callbacks
-    def on_scroll_view_scroll(self, instance, value):
-        _ = instance
-        self.slider.unbind(value=self.adjust_scroll_view)
-        self.slider.value = value
-        self.slider.bind(value=self.adjust_scroll_view)
 
-
-    def adjust_scroll_view(self, instance, value):
-        _ = instance
-        self.scroll_view.scroll_y = value
 
 
     def produce_dataset_files(self, *args):
--- data_collector/elements/selection.py
+++ data_collector/elements/selection.py
@@ -1,49 +1,114 @@
 from __future__ import annotations
 
-# from kivy.uix.boxlayout import BoxLayout
+from typing import Optional
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
-
-from data_collector.elements.types import BlackLabel
-from data_collector.elements.fsnode_widget import NodeWidget
-from data_collector.filesystem.fsnode import FsNode
-
-# from kivy.uix.widget import Widget
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 
 from data_collector.resources import get_logo_path
+from data_collector.filesystem.fsnode import FsNode
+from data_collector.elements.fsnode_widget import NodeWidget
+from data_collector.elements.types import ThickVerticalSlider, BlackLabel
+
+from data_collector.configs import get_line_height
 
 # -------------------------------------------
 # widgets
 
-def get_header_widget(num_elements: int) -> Widget:
-    with_leading_dot_list = [f'.{xrd_format}' for xrd_format in FsNode.default_xrd_formats]
+class SelectionLayout(BoxLayout):
+    def __init__(self, rootCheckbox : NodeWidget, **kwargs):
+        super(SelectionLayout, self).__init__(orientation='horizontal', size_hint=(1, 0.9), **kwargs)
 
-    file_count_label = BlackLabel(
-        text=f"Found {num_elements} files that match specified XRD formats:\n {with_leading_dot_list}",
-        size_hint=(0.6, 1),  # Adjusted size hint
-        halign="center",
-        valign="middle"
-    )
-    file_count_label.bind(size=file_count_label.setter('text_size'))
+        self.header_layout = self.get_header_widget(num_elements=0)
+        self.slider = ThickVerticalSlider(orientation='vertical', min=0, max=1, value=1, size_hint=(0.1, 1))
 
-    logo_image = Image(source=get_logo_path(), size_hint=(0.3, 1))
+        self.scroll_view : Optional[Widget] = None
+        self.root_checkbox = rootCheckbox
 
-    # Transparent placeholders to center the logo
-    left_placeholder = Widget(size_hint=(0.4, 1))
-    right_placeholder = Widget(size_hint=(0.1, 1))
+        self.scroll_view = self.get_scroll_view()
 
-    layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.125))
-    layout.add_widget(left_placeholder)  # Placeholder to balance the layout
-    layout.add_widget(file_count_label)
-    layout.add_widget(right_placeholder)  # Placeholder to balance the layout
-    layout.add_widget(logo_image)
+        self.header_layout = self.get_header_widget(num_elements=0)
+        self.header_layout.opacity = 0
+        self.checkboxes_layout = self.get_checkboxes_layout(file_count_label=self.header_layout,
+                                                  scroll_view=self.scroll_view)
 
-    return layout
+        self.scroll_view.bind(scroll_y=self.on_scroll_view_scroll)
+        self.slider.bind(value=self.adjust_scroll_view)
+
+        self.add_widget(self.checkboxes_layout)
+        self.add_widget(self.slider)
+
+
+
+    def set_content(self, path : str):
+        self.root_checkbox: NodeWidget = NodeWidget(path=path, height=get_line_height(), scroll_view=self.scroll_view)
+        self.root_checkbox.recursively_initialize_filestructure()
+
+        new_label = self.get_header_widget(num_elements=len(self.root_checkbox.xrd_file_des)).children[2]
+        self.header_layout.children[2].text =  new_label.text
+        self.header_layout.opacity = 1
+
+        scroll_layout = get_scroll_layout(root_checkbox=self.root_checkbox)
+        self.scroll_view.add_widget(widget=scroll_layout)
+
+
+    @staticmethod
+    def get_header_widget(num_elements: int) -> Widget:
+        with_leading_dot_list = [f'.{xrd_format}' for xrd_format in FsNode.default_xrd_formats]
+
+        file_count_label = BlackLabel(
+            text=f"Found {num_elements} files that match specified XRD formats:\n {with_leading_dot_list}",
+            size_hint=(0.6, 1),  # Adjusted size hint
+            halign="center",
+            valign="middle"
+        )
+        file_count_label.bind(size=file_count_label.setter('text_size'))
+
+        logo_image = Image(source=get_logo_path(), size_hint=(0.3, 1))
+
+        # Transparent placeholders to center the logo
+        left_placeholder = Widget(size_hint=(0.4, 1))
+        right_placeholder = Widget(size_hint=(0.1, 1))
+
+        layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.125))
+        layout.add_widget(left_placeholder)  # Placeholder to balance the layout
+        layout.add_widget(file_count_label)
+        layout.add_widget(right_placeholder)  # Placeholder to balance the layout
+        layout.add_widget(logo_image)
+
+        return layout
+
+    def on_scroll_view_scroll(self, instance, value):
+        _ = instance
+        self.slider.unbind(value=self.adjust_scroll_view)
+        self.slider.value = value
+        self.slider.bind(value=self.adjust_scroll_view)
+
+
+    def adjust_scroll_view(self, instance, value):
+        _ = instance
+        self.scroll_view.scroll_y = value
+
+    @staticmethod
+    def get_checkboxes_layout(file_count_label: Label, scroll_view: ScrollView):
+        select_layout = BoxLayout(orientation='vertical')
+        select_layout.add_widget(widget=file_count_label)
+        select_layout.add_widget(widget=scroll_view)
+
+        return select_layout
+
+    @staticmethod
+    def get_scroll_view():
+        scroll_view = ScrollView(size_hint=(1, 1), bar_width=0)
+        scrol_unit = 200
+        scroll_view.scroll_distance = scrol_unit
+        scroll_view.scroll_wheel_distance = scrol_unit
+
+        return scroll_view
 
 
 def get_feedback_widget(font_size : float) -> Widget:
@@ -60,24 +125,12 @@ def get_ok_button() -> Widget:
 # -------------------------------------------
 # layouts
 
-def get_checkboxes_layout(file_count_label: Label, scroll_view: ScrollView):
-    select_layout = BoxLayout(orientation='vertical')
-    select_layout.add_widget(widget=file_count_label)
-    select_layout.add_widget(widget=scroll_view)
-
-    return select_layout
 
 
-def get_scroll_view():
-    scroll_view = ScrollView(size_hint=(1, 1), bar_width=0)
-    scrol_unit = 200
-    scroll_view.scroll_distance = scrol_unit
-    scroll_view.scroll_wheel_distance = scrol_unit
-
-    return scroll_view
 
 
-def get_scrollable_checkboxes_layout(root_checkbox : NodeWidget):
+
+def get_scroll_layout(root_checkbox : NodeWidget):
     scroll_layout = BoxLayout(orientation='vertical', size_hint_y=None)
     scroll_layout.bind(minimum_height=scroll_layout.setter('height'))
     recursively_add_boxes(gui_parent=scroll_layout, root_box=root_checkbox, indent=0)
