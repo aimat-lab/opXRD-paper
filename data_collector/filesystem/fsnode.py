934b9ee0d80dede6424b2f15e7f5999943ae995b
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-01-16 16:04:19 +0100

    data_collector: Added get_filestructure via os.walk method

--- data_collector/filesystem/fsnode.py
+++ data_collector/filesystem/fsnode.py
@@ -76,3 +76,20 @@ class FsNode:
     def _get_subnodes(self, criterion : callable):
         return [path for path in self.sub_paths if criterion(path)]
 
+    def get_file_structure(self):
+        root_dir = self.path
+        file_structure = {}
+
+        for root, dirs, files in os.walk(root_dir):
+            relative_path = root.replace(root_dir, '').lstrip(os.sep)
+            current_level = file_structure
+
+            parts = relative_path.split(os.sep)
+            for part in parts:
+                if part:
+                    current_level = current_level.setdefault(part, {})
+
+            for file in files:
+                current_level[file] = None
+
+        return file_structure
