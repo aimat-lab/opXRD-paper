a33a5fd5dbc2b9e4a6fe5e1fd0925677b84b75a6
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-01-19 21:39:51 +0100

    data_collect: Updated packaging script to build in folder in home directory

--- data_collector/package.sh
+++ data_collector/package.sh
@@ -1,1 +1,16 @@
-pyinstaller prod_run.spec
+#!/bin/bash
+
+# Define the root directory where the script and build should be located
+ROOT_DIR="$HOME/xrd_data_collect"
+
+if [ ! -d "$ROOT_DIR" ]; then
+    mkdir -p "$ROOT_DIR"
+fi
+
+
+DIST_DIR="$ROOT_DIR/dist"
+BUILD_DIR="$ROOT_DIR/build"
+
+pyinstaller prod_run.spec --distpath "$DIST_DIR" --workpath "$BUILD_DIR"
+echo "-> Built executable at: $ROOT_DIR/dist/your_app_name"
+echo "done"
